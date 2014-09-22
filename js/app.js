521e89c5d9790b15484381dba0852b2fd052d862
John J Czaplewski <jczaplew@gmail.com>
2014-09-22 16:04:35 -0500

    Towards responsive

--- js/app.js
+++ js/app.js
@@ -1,52 +1,53 @@
-  /* Via http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript */
-  function is_touch_device() {
-    return 'ontouchstart' in window || 'onmsgesturechange' in window;
-  };
-  $(document).ready(function() {
-    if (is_touch_device()) {
+/* Via http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript */
+function is_touch_device() {
+  return 'ontouchstart' in window || 'onmsgesturechange' in window;
+};
+$(document).ready(function() {
+  if (is_touch_device()) {
 
-    }
-  });
-    
+  }
+});
+  
 
-  var snapper = new Snap({
-    element: document.getElementById("snapContent"),
-    disable: 'left',
-    touchToDrag: false
-  });
+var snapper = new Snap({
+  element: document.getElementById("snapContent"),
+  disable: 'left',
+  touchToDrag: false
+});
 
-  $("#openRight").click(function() {
+$("#openRight").click(function() {
+  if (snapper.state().state === "right") {
+    snapper.close();
+  } else {
+    snapper.open("right");
+  }
+});
+
+$(".drawer-menu").click(function() {
+  snapper.close();
+});
+
+var checkWindowSize = (function checkWindowSizeF() {
+  if (window.innerWidth > 768) {
     if (snapper.state().state === "right") {
       snapper.close();
-    } else {
-      snapper.open("right");
     }
-  });
+    snapper.disable();
+  } else {
+    snapper.enable();
+  }
+  return checkWindowSizeF;
+}());
 
-  $(".drawer-menu").click(function() {
-    snapper.close();
-  });
+$(window).on("resize", checkWindowSize);
 
-  var checkWindowSize = (function checkWindowSizeF() {
-    if (window.innerWidth > 768) {
-      if (snapper.state().state === "right") {
-        snapper.close();
-      }
-      snapper.disable();
-    } else {
-      snapper.enable();
-    }
-    return checkWindowSizeF;
-  }());
+$.getJSON("http://dev.macrostrat.org/api/stats?all", function(data) {
+  for (var i = 0; i < data.success.data.length; i++) {
+    var place = data.success.data[i].project.toLowerCase().replace(" ", "-");
+    $("#" + place + "-stats").html(data.success.data[i].packages + " packages. " + data.success.data[i].units + " units. " + data.success.data[i].pbdb_collections + " collections.");
+  }
+});
 
-  $(window).on("resize", checkWindowSize);
-
-  $.getJSON("http://dev.macrostrat.org/api/stats?all", function(data) {
-    for (var i = 0; i < data.success.data.length; i++) {
-      var place = data.success.data[i].project.toLowerCase().replace(" ", "-");
-      $("#" + place + "-stats").html(data.success.data[i].packages + " packages. " + data.success.data[i].units + " units. " + data.success.data[i].pbdb_collections + " collections.");
-    }
-  });
-
-
-
+window.addEventListener('load', function() {
+    FastClick.attach(document.body);
+}, false);
