07da9c627b8b9860e1e541cefe8b57aa7b75da5a
John J Czaplewski <jczaplew@gmail.com>
2015-09-25 10:55:38 -0500

    Tons of updates

--- js/app.js
+++ js/app.js
@@ -1,53 +1,91 @@
-/* Via http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript */
-function is_touch_device() {
-  return 'ontouchstart' in window || 'onmsgesturechange' in window;
-};
-$(document).ready(function() {
-  if (is_touch_device()) {
+(function() {
+  /* Via http://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript */
+  function is_touch_device() {
+    return 'ontouchstart' in window || 'onmsgesturechange' in window;
+  };
 
+  function addCommas(obj) {
+    function commaize(x) {
+      x = parseInt(x);
+      var parts = x.toString().split(".");
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return parts.join(".");
+    }
+    if (typeof(obj) === "object") {
+      Object.keys(obj).forEach(function(j) {
+        if (typeof(obj[j]) === "number" && j != "id" && j != "col_id" && j != "unit_id" && j != "strat_name_id") {
+          obj[j] = commaize(obj[j])
+        }
+      });
+      return obj;
+    } else {
+      return commaize(obj);
+    }
   }
-});
-  
 
-var snapper = new Snap({
-  element: document.getElementById("snapContent"),
-  disable: 'left',
-  touchToDrag: false
-});
+  $(document).ready(function() {
+    if (is_touch_device()) {
 
-$("#openRight").click(function() {
-  if (snapper.state().state === "right") {
-    snapper.close();
-  } else {
-    snapper.open("right");
-  }
-});
+    }
+  });
 
-$(".drawer-menu").click(function() {
-  snapper.close();
-});
 
-var checkWindowSize = (function checkWindowSizeF() {
-  if (window.innerWidth > 768) {
+  var snapper = new Snap({
+    element: document.getElementById("snapContent"),
+    disable: 'left',
+    touchToDrag: false
+  });
+
+  $("#openRight").click(function() {
     if (snapper.state().state === "right") {
       snapper.close();
+    } else {
+      snapper.open("right");
     }
-    snapper.disable();
-  } else {
-    snapper.enable();
-  }
-  return checkWindowSizeF;
-}());
+  });
 
-$(window).on("resize", checkWindowSize);
+  $(".drawer-menu").click(function() {
+    snapper.close();
+  });
 
-$.getJSON("//dev.macrostrat.org/api/stats?all", function(data) {
-  for (var i = 0; i < data.success.data.length; i++) {
-    var place = data.success.data[i].project.toLowerCase().replace(" ", "-");
-    $("#" + place + "-stats").html(data.success.data[i].packages + " packages. " + data.success.data[i].units + " units. " + data.success.data[i].pbdb_collections + " collections.");
-  }
-});
+  var checkWindowSize = (function checkWindowSizeF() {
+    if (window.innerWidth > 768) {
+      if (snapper.state().state === "right") {
+        snapper.close();
+      }
+      snapper.disable();
+    } else {
+      snapper.enable();
+    }
+    return checkWindowSizeF;
+  }());
 
-window.addEventListener('load', function() {
-    FastClick.attach(document.body);
-}, false);
+  $(window).on("resize", checkWindowSize);
+
+  $.getJSON("//dev.macrostrat.org/api/stats?all", function(data) {
+    var summary = {
+        columns: 0,
+        packages: 0,
+        units: 0,
+        collections: 0,
+        measurements: 0
+      }
+    for (var i = 0; i < data.success.data.length; i++) {
+      summary.columns += data.success.data[i].columns;
+      summary.packages += data.success.data[i].packages;
+      summary.units += data.success.data[i].units;
+      summary.collections += data.success.data[i].pbdb_collections;
+      summary.measurements += data.success.data[i].measurements;
+
+      var place = data.success.data[i].project.toLowerCase().replace(" ", "-");
+      $("#" + place + "-stats").html(addCommas(data.success.data[i].packages) + " packages. " + addCommas(data.success.data[i].units) + " units. " + addCommas(data.success.data[i].pbdb_collections) + " collections.");
+    }
+
+    $("#top-stats").html($("#top-stats").html() + addCommas(summary.columns) + " columns, " + addCommas(summary.packages) + " packages, and " + addCommas(summary.units) + " units");
+  });
+
+  window.addEventListener("load", function() {
+      FastClick.attach(document.body);
+  }, false);
+
+})()
